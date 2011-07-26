2d44886a48e59258515f166750fc450b938ee9a0
John Leung <jklemail@gmail.com>
2011-07-26 16:24:02 -0700

    renamed:    README.txt -> README 	modified:   generate_spe.py, README

--- generate_spe.py
+++ generate_spe.py
@@ -6,7 +6,7 @@ file_list = \
  "spe_base.js math.js vector.js world.js collision.js group.js particle.js shapes.js".split()
 
 outfile = open(outfile_str, 'w')
-outfile.write("// Simple Physics Engine (generated single file)\n\n")
+outfile.write("// Simple Physics Engine (generated single source file)\n\n")
 
 for f in file_list:
     outfile.write(
